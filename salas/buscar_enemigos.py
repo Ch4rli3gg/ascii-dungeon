c006bded0693fa82020c3b10a7ea41e55abd7af0
Ch4rli3gg <95308151+Ch4rli3gg@users.noreply.github.com>
2026-06-08 14:06:50 -0600

    Implementa lectura de enemigos desde mapas

---
+++ salas/buscar_enemigos.py
@@ -0,0 +1,33 @@
+from leer_salas import cargar_mapa
+
+#mapa = cargar_mapa("sala_enemigos.txt")
+
+def buscar_enemigos(mapa):
+    
+    enemigos=[]
+    
+    for y in range(len(mapa)):
+        
+        for x in range(len(mapa[y])):
+            
+            if mapa[y][x] == "E":
+                
+                enemigos.append({
+                    "x": x,
+                    "y": y,
+                    "tipo": "normal"
+                })
+                
+            elif mapa[y][x] == "C":
+                enemigos.append({
+                    "x": x,
+                    "y": y,
+                    "tipo": "loco"
+                })
+                
+    return enemigos
+            
+    
+    
+#print(buscar_enemigos(mapa))
+    
